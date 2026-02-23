a0f10df9af8e136d99ba11f5a11fb831b2272ba0
thecoolestfali <baszucki906@gmail.com>
2026-02-23 15:23:38 +0300

    Add files via upload

---
+++ VenturaAPI/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace VenturaAPI
+{
+    public class VenturaAPI
+    {
+        public static void DisallowRun(string processname)
+        {
+            Microsoft.Win32.Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\DisallowRun").SetValue(processname, processname);
+        }
+    }
+}
